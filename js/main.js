ac79d5a9163d1fcc6cf45f4fe62ba492c7025011
Sukyung Cho <doradora523@naver.com>
2023-01-26 21:50:29 +0900

    feat: Create setLayout and scrollLoop functions

--- js/main.js
+++ js/main.js
@@ -1,4 +1,9 @@
 (() => {
+
+  let yOffset = 0; // window.pageYOffset 대신 쓸 변수
+  let prevScrollHeight = 0; // 현재 스크롤 위치(yOffset)보다 이전에 위치한 스크롤 섹션들의 스크롤 높이값의 합
+  let currentScene = 0; // 현재 활성화 된(눈 앞에 보고있는) 씬(scroll-section)
+
   const sceneInfo = [
     {
       // 0
@@ -42,11 +47,52 @@
     // 각 스크롤 섹션의 높이 세팅
     for (let i = 0; i < sceneInfo.length; i++) {
       sceneInfo[i].scrollHeight = sceneInfo[i].heightNum * window.innerHeight;
+      // @ts-ignore
       sceneInfo[
         i
+        // @ts-ignore
       ].objs.container.style.height = `${sceneInfo[i].scrollHeight}px`;
+    }
+
+    yOffset = window.pageYOffset;
+
+    let totalScrollHeight = 0;
+    for (let i = 0; i < sceneInfo.length; i++) {
+      totalScrollHeight += sceneInfo[i].scrollHeight;
+      if (totalScrollHeight >= yOffset) {
+        currentScene = i;
+        break;
+      }
+    }
+
+    document.body.setAttribute("id", `show-scene-${currentScene}`);
+  }
+
+  function scrollLoop() {
+    prevScrollHeight = 0;
+
+    for (let i = 0; i < currentScene; i++) {
+      prevScrollHeight += sceneInfo[i].scrollHeight;
+    }
+
+    if (yOffset > prevScrollHeight + sceneInfo[currentScene].scrollHeight) {
+      currentScene++;
+      document.body.setAttribute("id", `show-scene-${currentScene}`);
+    }
+
+    if (yOffset < prevScrollHeight) {
+      if (currentScene === 0) return; // bouncing 효과로 인해 scene이 마이너스가 되는 것을 방지
+      currentScene--;
+      document.body.setAttribute("id", `show-scene-${currentScene}`);
     }
   }
 
-  setLayout();
+  window.addEventListener("scroll", () => {
+    yOffset = window.pageYOffset;
+    scrollLoop();
+  });
+  // window.addEventListener('DOMContentLoaded', setLayout); // HTML이 로딩되면 실행
+  window.addEventListener("load", setLayout); // WEB에 있는 IMG 등의 리소스까지 로딩되면 실행
+  window.addEventListener("resize", setLayout);
+
 })();
